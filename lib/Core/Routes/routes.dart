0d2cc53d761fedf12e9c341012b201c80677d485
MOHAMAD\moham <mohamadgazer@yahoo.com>
2024-03-09 07:10:37 +0200

    -set core folder -set routes folder in core. -set routes class. in routes.dart

---
+++ lib/Core/Routes/routes.dart
@@ -0,0 +1,8 @@
+class Routes {
+  static const String home = "/";
+  static const String camera = "/camera";
+  static const String uploadVideo = "/upload_video";
+  static const String userView = "/userView";
+  static const String editUser = "/edit_user";
+  static const String settings = "/settings";
+}
