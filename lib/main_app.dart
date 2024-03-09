f9d3b38a85b7b6e1fe43e8ac441e4d86fa2181f9
MOHAMAD\moham <mohamadgazer@yahoo.com>
2024-03-09 07:38:03 +0200

    - add routes to mainApp. -add routes to main development. -add routes to main production.

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_flutter_clone/Core/Routes/app_router.dart';
 
 class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+  final AppRouter appRouter;
+  const MainApp({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      onGenerateRoute: appRouter.genrateRoute,
     );
   }
 }
