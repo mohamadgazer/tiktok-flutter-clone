6a9a8a605f981f58bb8575fafc499c6e9d5acf62
MOHAMAD\moham <mohamadgazer@yahoo.com>
2024-03-09 09:28:53 +0200

    - add bloc observer to main_development.dart.

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tiktok_flutter_clone/Core/Routes/app_router.dart';
+import 'package:tiktok_flutter_clone/Core/helper/bloc_observer.dart';
 
 import 'main_app.dart';
 
@@ -9,6 +11,7 @@ void main() {
     systemNavigationBarColor: Colors.amber,
     statusBarColor: Colors.red,
   ));
+  Bloc.observer = SimpleBlocObserver();
   runApp(MainApp(
     appRouter: AppRouter(),
   ));
