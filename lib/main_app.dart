113645b562782902af1fdae9b1305a968b96276b
MOHAMAD\moham <mohamadgazer@yahoo.com>
2024-03-09 09:38:47 +0200

    - add deviec preview to main app.

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_flutter_clone/Core/Localization/app_local.dart';
 import 'package:tiktok_flutter_clone/Core/Routes/app_router.dart';
@@ -17,6 +18,8 @@ class MainApp extends StatelessWidget {
       // localiztion
       localizationsDelegates: localizationsDelegates,
       supportedLocales: S.delegate.supportedLocales,
+      //deview preview
+      builder: DevicePreview.appBuilder,
     );
   }
 }
