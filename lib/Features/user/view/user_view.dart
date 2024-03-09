949817b2e3e572a54683b4c3c48bc31d53deb019
MOHAMAD\moham <mohamadgazer@yahoo.com>
2024-03-09 07:30:03 +0200

    - add features folder. - add home , camera , user, edit user ,uplod view and setting page folder and view dart files. - add appRouter file  in Routes folder.

---
+++ lib/Features/user/view/user_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class UserView extends StatelessWidget {
+  const UserView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Page'),
+      ),
+      body: const Center(
+        child: Text('This is the User Page'),
+      ),
+    );
+  }
+}
