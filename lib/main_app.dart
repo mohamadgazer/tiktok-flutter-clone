d09d7ed713f214034ef89eb7f35a4c52eca7107f
MOHAMAD\moham <mohamadgazer@yahoo.com>
2024-03-09 07:44:12 +0200

    - add helper folder in core. - add extiontions in helper.

--- lib/main_app.dart
+++ lib/main_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_flutter_clone/Core/Routes/app_router.dart';
+import 'package:tiktok_flutter_clone/Core/Routes/routes.dart';
 
 class MainApp extends StatelessWidget {
   final AppRouter appRouter;
@@ -8,6 +9,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: Routes.home,
       onGenerateRoute: appRouter.genrateRoute,
     );
   }
