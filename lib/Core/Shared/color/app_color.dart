0a1c96577a2c2b45141306b4aae0ec4a3017d6ed
MOHAMAD\moham <mohamadgazer@yahoo.com>
2024-03-10 04:25:17 +0200

    - add color manger.

---
+++ lib/Core/Shared/color/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColorManger {
+  static const Color tiktokDark = Color(0xff161722);
+  static const Color tiktokLightBlue = Color(0xff65D2E9);
+  static const Color tiktokPink = Color(0xffE6436D);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color fBFBFB = Color(0xffFBFBFB);
+  static Color white60 = const Color.fromRGBO(255, 255, 255, 60);
+  static Color white20 = const Color.fromRGBO(255, 255, 255, 20);
+  static const Color aBABAB = Color(0xffABABAB);
+  static const Color black = Color(0xff000000);
+}
