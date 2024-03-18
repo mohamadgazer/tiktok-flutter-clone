c6f9d77c22051198fdbe1e6fc6b85eb233e2c658
mohamad gazer <37588351+mohamadgazer@users.noreply.github.com>
2024-03-18 03:08:21 +0200

    Revert "Navigationbar"

--- lib/Features/tabs/cubit_tabs/tabs_view_state.dart
+++
@@ -1,8 +0,0 @@
-part of 'tabs_view_cubit.dart';
-
-@immutable
-sealed class TabsViewState {}
-
-final class TabsViewInitial extends TabsViewState {}
-
-final class TabsChangedState extends TabsViewState {}
